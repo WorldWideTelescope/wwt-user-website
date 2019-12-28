ab4c2d48f0e5133758fdf3f7aa300be59a0e1a2e
Peter Williams <peter@newton.cx>
2019-12-28 17:29:26 -0500

    Populate with a VS2015 ASP.NET template
    Holw COW it is heavyweight.

---
+++ WWTUserWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WWTUserWebsite.MvcApplication" Language="C#" %>
---
+++ WWTUserWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WWTUserWebsite.Startup))]
+namespace WWTUserWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
